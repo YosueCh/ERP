37caf8b2f4eb366d8d23ccf67294baa2d3901a24
Yosue <s.yosue.chavez@gmail.com>
2026-02-17 10:00:00 -0600

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ButtonModule,RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
